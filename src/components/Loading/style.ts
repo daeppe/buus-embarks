e717007f110b0e16cd52fbb3b30c7e3759fd1509
Epichin <d.pena@hotmail.com>
2023-03-24 20:32:36 -0300

    feat(loading-component): addded loading component.

---
+++ src/components/Loading/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const LoadingStyled = styled.svg`
+width: 100px;
+height: 100px;
+margin: 20px;
+display:inline-block;
+`;
--- src/pages/Embarks/index.tsx
+++ src/pages/Embarks/index.tsx
@@ -7,6 +7,7 @@ import { EmbarksType } from '../../types/Embarks';
 import DataTable from '../../components/DataTable/DataTable';
 import { EmbarksTable, HeaderBar, SelectedDates } from './styles';
 import { Button } from '@mui/material';
+import { Loading } from '../../components/Loading/Loading';
 
 type selectedDatesType = {
     startDate: Date | null;
@@ -16,16 +17,23 @@ type selectedDatesType = {
 export const Embarks = () => {
     const [selectedDates, setSelectedDates] = useState<selectedDatesType>({} as selectedDatesType);
     const [chosenEmbarks, setChosenEmbarks] = useState<Array<EmbarksType>>([] as Array<EmbarksType>);
+    const [removeLoading, setRemoveLoading] = useState<boolean>(true);
     const navigate = useNavigate();
     const auth = useContext(AuthContext);
     const embark = useContext(EmbarksContext);
 
     useEffect(() => {
+        if (selectedDates.startDate && selectedDates.endDate) {
+            setRemoveLoading(false);
+        }
         const isValidDates = embark.validateData(selectedDates.startDate!, selectedDates.endDate!);
         if (isValidDates) {
             const embarksResult = embark.possibleShipments(selectedDates.startDate!, selectedDates.endDate!);
             setChosenEmbarks(embarksResult);
         }
+        setTimeout(() => {
+            setRemoveLoading(true);
+        }, 3000);
     }, [selectedDates]);
 
     const handleStartDateTimeSelected = (selectedDate: Date | null) => {
@@ -47,12 +55,16 @@ export const Embarks = () => {
             <h2>Tela Embarks</h2>
             <Button onClick={handleLogout} variant="contained" type='submit'>Logout</Button>
         </HeaderBar>
+
         <SelectedDates>
             <DatePicker onSelectedDate={handleStartDateTimeSelected} />
             <DatePicker onSelectedDate={handleEndDateTimeSelected} />
         </SelectedDates>
         <EmbarksTable>
-            <DataTable chosenEmbarks={chosenEmbarks} />
+            {removeLoading ?
+                <DataTable chosenEmbarks={chosenEmbarks} /> :
+                <Loading />
+            }
         </EmbarksTable>
     </>
     );
